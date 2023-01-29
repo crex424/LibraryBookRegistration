a791c08d4f043daf92a3066edf2684a5a8db2a64
TUNG N KIM <Tungkims@gmail.com>
2023-01-29 03:34:04 -0600

    Initialized Registration class

---
+++ LibraryBookRegistration/Registration.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryBookRegistration
+{
+    /// <summary>
+    /// Represents a Registration
+    /// </summary>
+    public class Registration
+    {
+        /// <summary>
+        /// Creates a Registration
+        /// </summary>
+        /// <param name="customerID"> Given CustomerID of a Customer </param>
+        /// <param name="isbn"> Given ISBN of a Book </param>
+        /// <param name="regDate"> Given registration date </param>
+        public Registration(int customerID, string isbn, DateTime regDate)
+        {
+            CustomerID = customerID;
+            ISBN = isbn;
+            RegDate = regDate;
+        }
+
+        /// <summary>
+        /// CustomerID of a Customer
+        /// </summary>
+        public int CustomerID { get; set; }
+
+        /// <summary>
+        /// ISBN of a Book
+        /// </summary>
+        public string ISBN { get; set; }
+
+        /// <summary>
+        /// Registration date
+        /// </summary>
+        public DateTime RegDate { get; set; }
+    }
+}
