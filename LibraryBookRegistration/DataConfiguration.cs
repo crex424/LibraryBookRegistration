6ebbc4e2342d5f2c69842443a1c9b8ed6efb9693
Campbell Rex <campbellrex@outlook.com>
2023-02-08 14:02:56 -0800

    Created RemoveWhiteSpace method

--- LibraryBookRegistration/DataConfiguration.cs
+++ LibraryBookRegistration/DataConfiguration.cs
@@ -9,6 +9,14 @@ namespace LibraryBookRegistration
 {
     public class DataConfiguration
     {
-
+        /// <summary>
+        /// Removes white space from the beginning and end of a string 
+        /// </summary>
+        /// <param name="text">the string to have white space removed</param>
+        /// <returns>A string with white space removed from the beginning and end of a string</returns>
+        public static string RemoveWhiteSpace(string text)
+        {
+            return text.Trim();
+        }
     }
 }
