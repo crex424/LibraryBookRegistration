2b28660464fd9910b9d84af0e9ea3b68d83c41a3
Campbell Rex <campbellrex@outlook.com>
2023-02-13 17:32:00 -0800

    Created add Customer method

--- LibraryBookRegistration/CustomerDB.cs
+++ LibraryBookRegistration/CustomerDB.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,5 +12,30 @@ namespace LibraryBookRegistration
     /// </summary>
     public class CustomerDB
     {
+        public static void Add(Customer c)
+        {
+            // establish connection to database
+            SqlConnection con = DBHelper.GetDatabaseConnection("BookRegistration");
+
+            // prepare insert statement
+            SqlCommand insertCmd = new SqlCommand();
+            insertCmd.Connection = con;
+            // Parameterize query
+            insertCmd.CommandText = "INSERT INTO Customer(DateOfBirth, FirstName, LastName, Title) " +
+                                              "VALUES (@dob, @firstName, @lastName, @title)";
+            insertCmd.Parameters.AddWithValue("@dob", c.DateOfBirth);
+            insertCmd.Parameters.AddWithValue("@firstName", c.FirstName);
+            insertCmd.Parameters.AddWithValue("@lastName", c.LastName);
+            insertCmd.Parameters.AddWithValue("title", c.Title);
+
+            // open connection to the database
+            con.Open();
+
+            // execute insert query
+            insertCmd.ExecuteNonQuery();
+
+            // close connection to the database
+            con.Close();
+        }
     }
 }
