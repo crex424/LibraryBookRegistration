ee5250924f0e43a33c4287e247f2c92b23d036f9
Campbell Rex <campbellrex@outlook.com>
2023-02-13 17:25:04 -0800

    Created CustomerDB class

---
+++ LibraryBookRegistration/CustomerDB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryBookRegistration
+{
+    /// <summary>
+    /// Represents CRUD Functionality of Customer
+    /// </summary>
+    public class CustomerDB
+    {
+    }
+}
