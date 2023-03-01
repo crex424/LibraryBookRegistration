8e12e68ce44bab8d83a35ee81428541d32c5c049
Campbell Rex <campbellrex@outlook.com>
2023-03-01 13:56:02 -0800

    Added method to populate Book ListView

--- LibraryBookRegistration/FrmManageBook.cs
+++ LibraryBookRegistration/FrmManageBook.cs
@@ -19,7 +19,26 @@ namespace LibraryBookRegistration
 
         private void FrmManageBook_Load(object sender, EventArgs e)
         {
+            lviBooks.Columns.Add("ISBN", 120, HorizontalAlignment.Left);
+            lviBooks.Columns.Add("Title", 450, HorizontalAlignment.Left);
+            lviBooks.Columns.Add("Price", 80, HorizontalAlignment.Left);
 
+
+            PopulateBookListView();
+        }
+
+        private void PopulateBookListView()
+        {
+            lviBooks.Items.Clear();
+
+            List<Book> books = BookDB.GetAllBooks();
+
+            foreach (Book currBook in books)
+            {
+                ListViewItem item = new(new[] { currBook.ISBN.ToString(), currBook.Title, currBook.Price.ToString() });
+                Tag = currBook;
+                lviBooks.Items.Add(item);
+            }
         }
     }
 }
